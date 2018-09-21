014b45c7f04e62be0a785893c21b1dc713fd4dcf
Nick Romans <nicholas.romans@gmail.com>
2018-09-21 16:50:02 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,19 @@
 class Owner
-  # code goes here
+  
+  attr_accessor :name, :pets
+  attr_reader
+  
+  @@all = []
+  
+
+  def initialize(name)
+    @pets = {fishes: [], cats: [], dogs: []}
+    @name = name
+    
+  end
+  
+  def self.all
+    @@all
+  end
+  
 end
